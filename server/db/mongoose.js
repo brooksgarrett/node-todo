b7313b649c0b0cbd4762532e17cf1a0aaf7d9661
Brooks Garrett <brooks@Brookss-MacBook-Pro.local>
2017-02-03 12:52:34 -0500

    Basic Todo POST endpoint

---
+++ server/db/mongoose.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+
+// Establish MongoDB Connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+module.exports = {mongoose};
---
+++ server/server.js
@@ -0,0 +1,29 @@
+// Package imports
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// Local imports
+var mongoose = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/api/v1/todos/', (req, res) => {
+    console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400);
+        res.send(e);
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server up on 3000');
+});
