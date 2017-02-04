ff181c7bdcff3ec4e1139e913c7a9cf6f4bd39e6
Brooks Garrett <brooks@brooksgarrett.com>
2017-02-03 21:48:49 -0500

    Fix typo in Mongo URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 // Establish MongoDB Connection
 mongoose.Promise = global.Promise;
-const dbUrl = process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp';
+const dbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
 mongoose.connect(dbUrl);
 
 module.exports = {mongoose};
