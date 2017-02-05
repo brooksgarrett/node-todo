cc3a7ba0cd11bfe3f63f0e6b3cf5963e81910a29
Brooks Garrett <brooks@brooksgarrett.com>
2017-02-04 22:26:26 -0500

    Add Patch todos by id route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 // Package imports
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+const _ = require('lodash')
 
 // Local imports
 var mongoose = require('./db/mongoose');
@@ -62,6 +63,41 @@ app.delete('/api/v1/todos/:id', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.patch('/api/v1/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, [
+        'text',
+        'completed'
+    ]);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(400)
+            .send({error: `${id} is not valid`});
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {
+        $set: body
+    }, {
+        new: true
+    }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(port, () => {
