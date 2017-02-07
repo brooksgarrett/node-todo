b61b99665eff2eef13073e3c3354aa15901b97c4
Brooks Garrett <brooks+git@brooksgarrett.com>
2017-02-07 12:04:24 -0500

    Remove debugger calls

--- server/models/user.js
+++ server/models/user.js
@@ -46,7 +46,6 @@ UserSchema.methods.generateAuthToken = function () {
     var access = 'auth';
 
     var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
-    debugger;
     user.tokens.push({access, token});
     return user.save().then(() => {
         return token;
