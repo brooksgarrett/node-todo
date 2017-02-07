a5cfc9666adcd6e6a7159299fce109c5d33797f0
Brooks Garrett <brooks+git@brooksgarrett.com>
2017-02-07 13:03:56 -0500

    Move config into config.json

--- config/config.js
+++ config/config.js
@@ -1,17 +1,9 @@
 var env = process.env.NODE_ENV || 'development';
 console.log(`Running in ${env}`);
 
-switch (env) {
-    case 'development':
-        process.env.PORT = 3000;
-        process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-        process.env.JWT_SECRET = 'abc123';
-        break;
-    case 'test':
-        process.env.PORT = 3000;
-        process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-        process.env.JWT_SECRET = 'abc123';
-        break;
-    case 'production':
-        break;
+if (env === 'development' || env === 'test') {
+    var config = require('./config.json')[env];
+    Object.keys(config).forEach((key) => {
+        process.env[key] = config[key];
+    });
 }
